5609ffab7359a0028584fe1b06c4df2fa166fcbd
luannarabeloo <luannarobertasocial@gmail.com>
2026-07-27 08:42:40 -0400

    feat: Cria rotas no arquivo controller para criar, listar e deletar, e desenvolve funções correspondentes no MissoesService

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,43 +1,43 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequestMapping("/missoes")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class MissaoController {
+public class MissoesController {
+
     private MissoesService missoesService;
 
+    public MissoesController(MissoesService missoesService) {
+        this.missoesService = missoesService;
+    }
+
     //Rota para criar uma missao
-    @PutMapping("/criar")
+    @PostMapping("/criar")
     public MissoesModel criarMissao(@RequestBody MissoesModel missoes){
         return missoesService.criarMissao(missoes);
     }
 
     //Rota pra listar todas as missoes
     @GetMapping("/listar")
-    public String listarMissoes() {
-        return "Listar todas as missoes";
+    public List<MissoesModel> listarMissoes() {
+        return missoesService.listarMissoes();
     }
+
     //Rota para listar missao por ID
     @GetMapping("/listar/{id}")
-    public String listarMissaoPorId(){
-        return "Listar missao por ID";
+    public MissoesModel listarMissaoPorId(@PathVariable Long id){
+        return missoesService.listarMissaoPorId(id);
     }
 
     //Rota para deletar missao por ID
-
+    @DeleteMapping("/deletar/{id}")
+    public void deletarMissaoPorId(@PathVariable Long id) {
+        missoesService.deletarMissaoPorId(id);
+    }
     //Rota para alterar missao por ID
 
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesService.java
@@ -1,4 +1,37 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class MissoesService {
+    private MissoesRepository missoesRepository;
+
+    public MissoesService(MissoesRepository missoesRepository) {
+        this.missoesRepository = missoesRepository;
+    }
+
+    //Criar missao
+    public MissoesModel criarMissao(MissoesModel missoes){
+        return missoesRepository.save(missoes);
+    }
+
+
+    //Listar todas as missoes
+    public List<MissoesModel> listarMissoes(){
+        return missoesRepository.findAll();
+    }
+
+    //Listar missao por Id
+    public MissoesModel listarMissaoPorId(Long id){
+        Optional<MissoesModel> missaoPorId = missoesRepository.findById(id);
+        return missaoPorId.orElse(null);
+    }
+
+    //Deletar missao por Id
+    public void deletarMissaoPorId(Long id) {
+        missoesRepository.deleteById(id);
+    }
 }
