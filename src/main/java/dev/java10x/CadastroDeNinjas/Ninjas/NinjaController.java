48868617df20b7d5d4057bd449597a623b815959
luannarabeloo <luannarobertasocial@gmail.com>
2026-08-05 21:37:03 -0400

    feat: Inclui DTO como abstracao para minha entidade na camada de Service e Controller

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -22,8 +22,8 @@ public class NinjaController {
 
     // Adiciona ninja   (CREATE)
     @PostMapping("/criar")
-    public NinjaModel criarNinja(@RequestBody NinjaModel ninja){
-        return ninjaService.criarNinja(ninja);
+    public NinjaDTO criarNinja(@RequestBody NinjaDTO ninjaDTO){
+        return ninjaService.criarNinja(ninjaDTO);
     }
 
     // Mostrar todos os ninjas    (READ)
